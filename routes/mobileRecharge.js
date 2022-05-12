f5d0805d5bb07a626498c6cca0ea9665f138a5dc
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-05-12 16:17:48 +0530

    done code

--- routes/mobileRecharge.js
+++ routes/mobileRecharge.js
@@ -18,7 +18,7 @@ router.post("/admin/mobile_recharge", mobile_recharge);
 router.get("/admin/gettransaction", gettransaction);
 router.get("/admin/getusertransaction/:id", getusertransaction);
 router.post("/admin/elec_paybill", elec_paybill);
-router.get("/admin/Elec_paybill", Elec_paybill);
+//router.get("/admin/Elec_paybill", Elec_paybill);
 
 
 // router.get("/admin/operators/auto-detect/:phone/:country", detectoperator);
